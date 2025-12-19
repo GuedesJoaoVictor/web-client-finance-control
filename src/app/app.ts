169ab0f17f1316e2a9958a00403511b1b208fb99
Gue10 <joaoguedes1404@gmail.com>
2025-12-19 18:08:04 -0300

    feat - Adicionado loading service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './pages/shared/header/header.component';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
+import {LoadingService} from './core/services/loading.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MatProgressSpinner],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('web-client');
+
+  constructor(protected loadingService: LoadingService) {}
 }
