defd0e7e96fa4e983cbb14a5487001da6aa02780
Gue10 <joaoguedes1404@gmail.com>
2026-01-08 17:38:13 -0300

    feat: initializing account links

---
+++ src/app/pages/portal-admin/account-links/account-links.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-account-links",
+    templateUrl: "./account-links.component.html",
+    styleUrls: ["./account-links.component.scss"],
+})
+export class AccountLinksComponent {
+
+    constructor() {}
+
+}
